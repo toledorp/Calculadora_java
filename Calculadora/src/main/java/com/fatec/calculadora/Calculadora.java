ad538ebe8b94c8d22babc2c8b13c8ef940287a7f
Rogerio Pupo Toledo <roge.pupo.toledo@gmail.com>
2025-09-23 21:36:49 -0300

    Add files via upload

---
+++ Calculadora/src/main/java/com/fatec/calculadora/Calculadora.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.fatec.calculadora;
+
+/**
+ *
+ * @author fatec-dsm2
+ */
+public class Calculadora {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
